d3f2ad9e8bf05dde07acb30513fe076272c24523
elemouellic <114568852+elemouellic@users.noreply.github.com>
2024-03-06 21:52:21 +0100

    update controllers

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -17,7 +17,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class CityController extends AbstractController
 {
 
-#[Route('/insertville', name: 'app_city_insert', methods: ['POST'])]
+    #[Route('/insertville', name: 'app_city_insert', methods: ['POST'])]
     public function insertCity(Request $request, EntityManagerInterface $em): JsonResponse
     {
         $data = Utils::serializeRequestData($request);
@@ -60,8 +60,69 @@ class CityController extends AbstractController
             'name' => $city->getName(),
             'zipcode' => $city->getZipcode(),
         ]);
+    }
 
+    #[Route('/deleteville/{id}', name: 'app_city_delete', methods: ['DELETE'])]
+    public function deleteCity(Request $request, $id, EntityManagerInterface $em): JsonResponse
+    {
 
+        // Get the city from the database
+        $city = $em->getRepository(City::class)->find($id);
+        if (!$city) {
+            throw new HttpException(404, 'City not found');
+        }
+
+        try {
+            $em->remove($city);
+            $em->flush();
+
+            return $this->json([
+                'message' => 'City deleted successfully',
+            ]);
+        } catch (Exception $e) {
+            throw new HttpException(400, "Error while deleting the city");
+        }
+    }
+
+    #[Route('/listeville', name: 'app_city_list', methods: ['GET'])]
+    public function listAllCities(EntityManagerInterface $em): JsonResponse
+    {
+        // Get all the cities from the database
+        $cities = $em->getRepository(City::class)->findAll();
+
+        // Initialize the array that will store the formatted data
+        $data = [];
+
+        // Loop through the cities and add the data to the array
+        foreach ($cities as $city) {
+            $data[] = [
+                'id' => $city->getId(),
+                'name' => $city->getName(),
+                'zipcode' => $city->getZipcode(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/listecodepostal', name: 'app_zipcode_list', methods: ['GET'])]
+    public function listAllZipCodes(EntityManagerInterface $em): JsonResponse
+    {
+        // Get all the zip codes from the database
+        $zipcodes = $em->getRepository(City::class)->findAll();
+
+        // Initialize the array that will store the formatted data
+        $data = [];
+
+        // Loop through the zip codes and add the data to the array
+        foreach ($zipcodes as $zipcode) {
+            $data[] = [
+                'id' => $zipcode->getId(),
+                'zipcode' => $zipcode->getZipcode(),
+            ];
+        }
+
+        return $this->json($data);
     }
 
 }
